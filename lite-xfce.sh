bb6bad3db895b8fc213cd8f306223236eed1acfb
catonrug <aigars.kadikis@gmail.com>
2018-05-20 20:12:11 +0300

    not tested

--- lite-xfce.sh
+++ lite-xfce.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+#apt-get update -y && apt-get install git -y && cd && git clone https://github.com/catonrug/rpi-stretch.git && cd rpi-stretch.git && ./lite-xfce.sh
+
 #update repos
 sudo apt-get update -y
 
